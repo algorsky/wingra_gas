95060a896121d9106f2869800d6bd0b6f721f3bc
hdugan <hilarydugan@gmail.com>
2026-06-17 11:49:47 -0500

    update map plots

--- Figure6.R
+++ Figure6.R
@@ -1,3 +1,4 @@
+library(cowplot)
 ################################# Polygons #################################
 source('0_PolygonsVoronoi.R')
 
@@ -7,23 +8,28 @@ polygon_plot <- ggplot() +
                                                           shape = as.factor(rake), fill = as.factor(rake), size = as.factor(rake)), stroke = 1.1) +
   geom_sf(data = sites_sf, color = 'black', fill = "yellow", size = 1.5, shape = 24, stroke = 0.5)+
   geom_sf(data = cluster_polygons, alpha = 0.2, linewidth = 0.5) +
-  scale_color_manual(valu                                                                  es = c("lightgray",'#c2e699',"#78c679",  "#006837", "gray77"),labels = c("0", "1", "2", "3", "Not sampled"),  name = "", guide = guide_legend(title.position = "top")) +
+  scale_color_manual(values = c("lightgray",'#c2e699',"#78c679",  "#006837", "gray77"),labels = c("0", "1", "2", "3", "Not sampled"),  name = "", guide = guide_legend(title.position = "top")) +
   scale_fill_manual(values = c("lightgray",'#c2e699',"#78c679",  "#006837", "gray77"),labels = c("0", "1", "2", "3", "Not sampled"), name = "", guide = guide_legend(title.position = "top")) +
   scale_shape_manual(values = c(19, 19, 19, 19, 4), labels = c("0", "1", "2", "3", "Not sampled"), name = "", guide = guide_legend(title.position = "top")) +
   scale_size_manual(values = c(0.2,0.2,0.2,0.2,0.2), labels = c("0", "1", "2", "3", "Not sampled"), name = "", guide = guide_legend(title.position = "top"))+
   # scale_y_continuous(breaks = c(43.05, 43.08)) +
   scale_x_continuous(breaks = c(-89.43, -89.42, -89.41))+
-  guides(color = guide_legend(override.aes = list(size = 5), title.position = "top", nrow = 1, byrow = TRUE))+
+  guides(color = guide_legend(override.aes = list(size = 2), title.position = "top", nrow = 1, byrow = TRUE))+
   theme_bw(base_size = 9)+
   theme(
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     legend.position = 'bottom',
-    legend.title = element_text(size=12),
-    legend.text = element_text(size=12),
-    legend.direction="horizontal"
+    legend.title = element_text(size=8),
+    legend.text = element_text(size=8),
+    legend.direction="horizontal",
+    legend.background = element_rect(fill = "transparent", colour = NA),
+    legend.box.background = element_rect(fill = "transparent", colour = NA),
+    legend.box.spacing = unit(0, "pt"),
+    legend.margin = margin(0,0,0,0),
+    legend.box.margin = margin(-4,0,0,0)
   ) +
-  ggspatial::annotation_scale( bar_cols = c("grey", "white"),  location = "br", size = )
+  ggspatial::annotation_scale( bar_cols = c("grey", "white"),  location = "br", text_cex = 0.5, pad_y = unit(0.15, "cm"))
 
 macrophyte<- read_csv("data/map/crosswalk_biomass.csv")
 co2_dissolved<- read_csv("data/co2_dissolved_use.csv")|>
@@ -38,7 +44,7 @@ co2_winter <- co2_dissolved%>%
 
 co2_winter_plot <- ggplot()+
   geom_smooth(data = filter(co2_winter, season == "ice"), aes(x = days_ice, y = CO2_mean, group = site, color = as.factor(biomass)), method = "lm", se = F)+
-  geom_vline(xintercept = 91, linetype = "dashed", linewidth = 1.4)+
+  geom_vline(xintercept = 91, linetype = "dashed", linewidth = 0.8)+
   geom_segment(aes(x = 61, xend = 91, y = -2.36 + 0.31*61, yend = -2.36 + 0.31*91), linetype ="dashed", color = "#F7FCF5")+
   geom_segment(aes(x = 61, xend = 91, y = 0.01 + 0.57*61, yend = 0.01 + 0.57*91), linetype ="dashed", color = "#A1D99B")+
   geom_segment(aes(x = 61, xend = 91, y = -8.68 + 0.53*61, yend = -8.68 + 0.53*91), linetype ="dashed", color = "#C7E9C0")+
@@ -47,10 +53,11 @@ co2_winter_plot <- ggplot()+
   geom_segment(aes(x = 61, xend = 91, y = -207 + 6.59*61, yend = -207 + 6.59*91), linetype ="dashed", color = "#238B45")+
   geom_segment(aes(x = 61, xend = 91, y = -174 + 5.48*61, yend = -174 + 5.48*91), linetype ="dashed", color = "#005A32")+
   geom_segment(aes(x = 61, xend = 91, y = -7.29 + 0.92*61, yend = -7.29 + 0.92*91), linetype ="dashed", color = "#74C476")+
-  geom_point(data = filter(co2_winter, season == "ice"), aes(x = days_ice, y = CO2_mean, fill = as.factor(biomass)), size = 2, shape = 21)+
-  geom_pointrange(data = filter(co2_winter, season == "ice"), aes(x = days_ice, y = CO2_mean, ymin = CO2_mean - CO2_sd, ymax = CO2_mean + CO2_sd, fill = as.factor(biomass)), shape = 21)+
+  geom_point(data = filter(co2_winter, season == "ice"), aes(x = days_ice, y = CO2_mean, fill = as.factor(biomass)), 
+    size = 2, shape = 21, stroke = 0.2)+
+  geom_pointrange(data = filter(co2_winter, season == "ice"), aes(x = days_ice, y = CO2_mean, ymin = CO2_mean - CO2_sd, ymax = CO2_mean + CO2_sd, fill = as.factor(biomass)), shape = 21, stroke = 0.2)+
   #geom_point(data = filter(co2_winter, season == "open"), aes(x = days_ice, y = CO2_mean, fill = as.factor(biomass)), size = 2, shape = 21)+
-  geom_pointrange(data = filter(co2_winter, season == "open"), aes(x = days_ice, y = CO2_mean, ymin = CO2_mean - CO2_sd, ymax = CO2_mean + CO2_sd,fill = as.factor(biomass)), shape = 21)+
+  geom_pointrange(data = filter(co2_winter, season == "open"), aes(x = days_ice, y = CO2_mean, ymin = CO2_mean - CO2_sd, ymax = CO2_mean + CO2_sd,fill = as.factor(biomass)), shape = 21, stroke = 0.2)+
   xlab("Days since ice-on")+
   scale_color_brewer(palette = "Greens")+
   scale_fill_brewer(palette = "Greens")+
@@ -58,8 +65,18 @@ co2_winter_plot <- ggplot()+
   theme_bw(base_size = 9)+
   theme(legend.position = 'none')
 
-polygon_plot + co2_winter_plot + 
-  plot_annotation(tag_levels = 'a', tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 9)) 
+# patchwork doesn't work well for maps 
+# polygon_plot + co2_winter_plot + plot_annotation(tag_levels = 'a', tag_prefix = "(", tag_suffix = ")") 
 
-ggsave(filename = 'figures/Figure6.png', width = 6, height = 4, units = 'in', dpi = 500)
+# ggsave(filename = 'figures/Figure6.png', width = 6, height = 2, units = 'in', dpi = 500)
+plot_grid(
+  polygon_plot +
+    theme(plot.tag = element_text(size = 8)),
+  co2_winter_plot +
+    theme(plot.tag = element_text(size = 8)),
+  labels = c("(a)", "(b)"),
+  label_size = 9,
+  label_fontface = "plain",
+  ncol = 2
+)
+ggsave(filename = 'figures/Figure6.png', width = 6, height = 2.5, units = 'in', dpi = 500, bg = 'white')
